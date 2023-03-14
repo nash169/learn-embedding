6c5354c1826517842fc163d3c95be3076fa6dd87
bernardo <bernardo.fichera@gmail.com>
2023-03-14 15:34:27 +0100

    Completed integration of convex/concave obstacle avoidance case.

--- learn_embedding/dynamics/__init__.py
+++ learn_embedding/dynamics/__init__.py
@@ -2,5 +2,6 @@
 
 from .first_geometry import FirstGeometry
 from .second_geometry import SecondGeometry
+from .linear_filed import LinearField
 
-__all__ = ["FirstGeometry", "SecondGeometry"]
+__all__ = ["FirstGeometry", "SecondGeometry", "LinearField"]
