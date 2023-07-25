2b66b0e7ab784693753a7b4e1548de9b0f56cae1
Bernardo <bernardo.fichera@gmail.com>
2023-07-25 16:26:56 +0200

    Fixed typo in the file name.

--- learn_embedding/dynamics/__init__.py
+++ learn_embedding/dynamics/__init__.py
@@ -2,6 +2,6 @@
 
 from .first_geometry import FirstGeometry
 from .second_geometry import SecondGeometry
-from .linear_filed import LinearField
+from .linear_field import LinearField
 
 __all__ = ["FirstGeometry", "SecondGeometry", "LinearField"]
