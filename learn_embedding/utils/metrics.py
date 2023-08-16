6ebd997bf9f50f277140109f0ff47dfa4847bf06
Bernardo <bernardo.fichera@gmail.com>
2023-08-16 12:08:17 +0200

    Minor.

--- learn_embedding/utils/metrics.py
+++ learn_embedding/utils/metrics.py
@@ -1,14 +1,15 @@
 import torch
+import numpy as np
 
 
-def metric_exp(y, y_obs, sigma, eta=1):
+def metric_exp(y, y_obs, sigma, eta=1.0):
     d = y-y_obs
     k = eta*torch.exp(-0.5*torch.sum(d.pow(2), dim=1) /
                       sigma ** 2).unsqueeze(1).unsqueeze(2)
     return torch.bmm(d.unsqueeze(2), d.unsqueeze(1)) * k.pow(2)/np.power(sigma, 4) + torch.eye(y.shape[1]).repeat(y.shape[0], 1, 1).to(y.device)
 
 
-def metric_infty(y, y_obs, a, b, r=0):
+def metric_infty(y, y_obs, a, b, r=1.0):
     d = y-y_obs
     k = (torch.norm(d, dim=1) - r).unsqueeze(1).unsqueeze(2)
     k = torch.exp(a/(b*torch.pow(k, b)))
