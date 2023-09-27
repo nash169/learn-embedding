f87e028be8a5458f142c9bc720002eac02ecf17f
Bernardo <bernardo.fichera@gmail.com>
2023-09-27 15:35:37 +0200

    Implemented directional and exponential dissipation.

--- learn_embedding/dynamics/second_geometry.py
+++ learn_embedding/dynamics/second_geometry.py
@@ -9,7 +9,7 @@ from ..covariances.spherical import Spherical
 
 
 class SecondGeometry(nn.Module):
-    def __init__(self, embedding, attractor, stiffness: Optional[nn.Module] = None,  dissipation: Optional[nn.Module] = None, field: Optional[nn.Module] = None, field_weight: Optional[float] = 1.0):
+    def __init__(self, embedding, attractor, stiffness: Optional[nn.Module] = Spherical(grad=False),  dissipation: Optional[nn.Module] = Spherical(grad=False)):
         super(SecondGeometry, self).__init__()
 
         # Embedding
@@ -19,21 +19,10 @@ class SecondGeometry(nn.Module):
         self._attractor = attractor
 
         # Stiffness matrix
-        if stiffness is not None:
-            self.stiffness = stiffness
-        else:
-            self.stiffness = Spherical(grad=False)
+        self.stiffness = stiffness
 
         # Dissipation matrix
-        if dissipation is not None:
-            self.dissipation = dissipation
-        else:
-            self.dissipation = Spherical(grad=False)
-
-        # Reference velocity field
-        if field is not None:
-            self.field = field
-            self.field_weight = field_weight
+        self.dissipation = dissipation
 
         # Velocity Dependent Embedding
         self._velocity_embedding = False
@@ -41,30 +30,45 @@ class SecondGeometry(nn.Module):
     # Forward Dynamics
     def forward(self, x):
         # data
-        pos = x[:, :int(x.shape[1]/2)]
-        vel = x[:, int(x.shape[1]/2):]
+        p = x[:, :int(x.shape[1]/2)]
+        v = x[:, int(x.shape[1]/2):]
+
         # embedding
-        y = self.embedding(pos, vel) if self.velocity_embedding else self.embedding(pos)
+        y = self.embedding(p, v) if self.velocity_embedding else self.embedding(p)
+
         # jacobian
-        j = self.embedding.jacobian(pos, y)
+        j = self.embedding.jacobian(p, y)
+
         # metric
         m = self.embedding.pullmetric(y, j)
+
         # christoffel
-        g = self.embedding.christoffel(pos, m)
-        # desired state
-        xd = pos - self.attractor
-        vd = vel - self.field_weight*self.field(pos) if hasattr(self, 'field') else vel
-        # dynamics harmonic components
+        g = self.embedding.christoffel(p, m)
+
+        # potential energy
+        f = self.stiffness(p - self.attractor)
+
+        # dissipation energy
+        f += self.dissipation(v)
+
+        # directional dissipation
+        if hasattr(self, 'field'):
+            f += self.field_weight*(v - self.field(p))
+
+        # exponential dissipation
+        if hasattr(self, 'exp_dissipation'):
+            f += self.exp_dissipation_weight*self.exp_dissipation(p, self.attractor.unsqueeze(0))*v
+
+        # dynamic harmonic components
         if hasattr(self.embedding, 'local_deformation'):
             harmonic_weight = torch.ones(x.shape[0], 1).to(x.device)
-            harmonic_weight[self.embedding.local_deformation(pos, vel) >= 0.01] = 0.0
-            # harmonic_weight[self.embedding.local_deformation(pos) >= 0.01] = 0.0
-            # print(harmonic_weight)
+            harmonic_weight[self.embedding.local_deformation(p, v) >= 0.01] = 0.0
+        else:
+            harmonic_weight = 1.0
 
-        # harmonic_weight = 0.0
+        f *= harmonic_weight
 
-        return (torch.bmm(m.inverse(), -(harmonic_weight*self.dissipation(vd)+harmonic_weight*self.stiffness(xd)).unsqueeze(2))
-                - torch.bmm(torch.einsum('bqij,bi->bqj', g, vel), vel.unsqueeze(2))).squeeze(2)
+        return (torch.bmm(m.inverse(), -f.unsqueeze(2)) - torch.bmm(torch.einsum('bqij,bi->bqj', g, v), v.unsqueeze(2))).squeeze(2)
 
     def geodesic(self, x):
         # data
